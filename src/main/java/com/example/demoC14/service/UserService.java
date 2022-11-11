11ed18ae00d6662018f30155fe827875d3719ac0
Nilufa Khan <khannilufa5@gmail.com>
2022-11-11 15:49:10 +0530

    Initial

---
+++ src/main/java/com/example/demoC14/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.demoC14.service;
+
+import com.example.demoC14.domain.User;
+import com.example.demoC14.exception.UserNotFoundException;
+
+import java.util.List;
+
+public interface UserService {
+    public User addUser(User user);
+    public User findByUserNameAndPassword(String userName, String password) throws UserNotFoundException;
+    List<User> getAllUser();
+    public boolean deleteByUserId(int userId) throws UserNotFoundException;
+}
---
+++ src/main/java/com/example/demoC14/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.demoC14.service;
+
+import com.example.demoC14.domain.User;
+import com.example.demoC14.exception.UserNotFoundException;
+import com.example.demoC14.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+    UserRepository userRepository;
+    @Autowired
+    public  UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findByUserNameAndPassword(String userName, String password) throws UserNotFoundException {
+        User user = userRepository.findByUserNameAndPassword(userName,password);
+        if(user==null){
+            throw new UserNotFoundException();
+        }
+        return user;
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Override
+    public boolean deleteByUserId(int userId) throws UserNotFoundException {
+        boolean result = false;
+        if(userRepository.findById(userId).isEmpty()){
+            throw new UserNotFoundException();
+        }else{
+           userRepository.deleteById(userId);
+            result = true;
+        }
+        return result;
+    }
+}
